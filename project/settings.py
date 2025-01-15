467be1706f25aff8f228de6a3e5197bc7fdad010
Flynotfly <Alteria2004@gmail.com>
2025-01-15 21:40:59 +0400

    change page after login to contract list

--- project/settings.py
+++ project/settings.py
@@ -127,6 +127,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
-LOGIN_REDIRECT_URL = 'dashboard'
+LOGIN_REDIRECT_URL = 'list_contract'
 LOGIN_URL = 'login'
 LOGOUT_URL = 'logout'
